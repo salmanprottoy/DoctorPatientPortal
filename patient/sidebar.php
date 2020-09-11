c295d0f1e475c071262ac634f6d2062b265a06f9
Md. Salman Hossan Prottoy <salman.prottoy@gmail.com>
2020-09-11 15:32:57 +0600

    patient update

---
+++ patient/sidebar.php
@@ -0,0 +1,44 @@
+<div class="wrapper">
+        <nav id="sidebar">
+            <ul class="list-unstyled components">
+                
+                <li class="active">
+                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                    <i class="fa fa-user"></i><span> Profile</span></a>
+                    <ul class="collapse list-unstyled" id="homeSubmenu">
+                        <li>
+                            <a href="patientInfo.php"><i class="fa fa-info-circle"></i><span> View Info</span></a>
+                        </li>
+                        <li>
+                            <a href="editInfo.php"><i class="fa fa-pencil-square-o" ></i><span> Edit Info</span></a>
+                        </li>
+                        <li>
+                            <a href="changePass.php"><i class="fa fa-key" ></i><span> Change Password</span></a>
+                        </li>
+                    </ul> 
+                </li>
+                
+                <li>
+                    <a href="Appointment.php"><i class="fas fa-calendar"></i><span> Appointment</span></a>
+                </li>
+                <li>
+                    <a href="Donation.php"><i class="fas fa-tint"></i><span>Organ Donation</span></a>
+                </li>
+                <li>
+                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                    <i class="fa fa-tasks"></i><span> Other Service</span></a>
+                    <ul class="collapse list-unstyled" id="pageSubmenu">
+                        <li>
+                            <a href="coronaUpdate.php"><i class="fa fa-medkit"></i><span> Corona Update</span></a>
+                        </li>
+                        <li>
+                            <a href="healthTips.php"><i class="fa fa-plus-square"></i><span> Health Tips</span></a>
+                        </li>
+                        <!-- <li>
+                            <a href="#">page3</a>
+                        </li> -->
+                    </ul> 
+                </li> 
+            </ul>   
+        </nav>
+        <div class="content">
