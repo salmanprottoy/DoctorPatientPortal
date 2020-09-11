6dd1209db7c0a8db9be96b19b50e0824197622e7
Md. Salman Hossan Prottoy <salman.prottoy@gmail.com>
2020-09-11 19:25:24 +0600

    Update index.php

--- patient/index.php
+++ patient/index.php
@@ -1,8 +1,3 @@
 <?php
-require "../includes/db_connect.inc.php";
-session_start();
-include('header.php');
-include('navbar.php');
-include('sidebar.php');
-include('patientprofile.php');
+include('patientProfile.php');
 ?>
